26e5223d29357be8109b4908351224fc2c730e9c
ercangercek@gmail.com <ercangercek@gmail.com>
2020-10-06 23:42:06 +0300

    added docker file

---
+++ public/precache-manifest.79f5c4adc0f3c7a4a9e61cc8e5f346df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95a377729000cd8e8350519131903c29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "697f51af8bcdc18f059b",
+    "url": "/static/css/main.4e6f81e3.chunk.css"
+  },
+  {
+    "revision": "36fd5a80501360a606c1",
+    "url": "/static/js/2.eb8e9afd.chunk.js"
+  },
+  {
+    "revision": "4db0b4b7fd2c7a0aaf4845dcd6ed7f11",
+    "url": "/static/js/2.eb8e9afd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "697f51af8bcdc18f059b",
+    "url": "/static/js/main.ce754e78.chunk.js"
+  },
+  {
+    "revision": "6ac0d3bfe9c69803ad2e",
+    "url": "/static/js/runtime-main.d04a9110.js"
+  }
+]);
